f41444c75461aaf442104fe2b4506fa46686e228
Alexander Schmutz <alexander.schmutz@partner.bmw.de>
2025-01-26 17:03:09 +0100

    Fix package name to call command with parameters

--- Package.swift
+++ Package.swift
@@ -4,14 +4,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "EasyFrameKitCLI",
+    name: "easy-frame",
     platforms: [.macOS(.v10_15)],
     dependencies: [
         .package(url: "https://github.com/ainame/FrameKit", from: "0.1.0"),
         .package(url: "https://github.com/apple/swift-argument-parser", from: "0.5.0"),
     ],
     targets: [
-        .executableTarget(name: "EasyFrameKitCLI", dependencies: [
+        .executableTarget(name: "easy-frame", dependencies: [
             .product(name: "FrameKit", package: "FrameKit"),
             .product(name: "ArgumentParser", package: "swift-argument-parser")
         ]),
