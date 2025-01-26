f2bc5c2f707ae3be6cca9719c0d0aea48f290803
Alexander Schmutz <alexander.schmutz@partner.bmw.de>
2025-01-26 16:26:38 +0100

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "EasyFrameKitCLI",
+    platforms: [.macOS(.v10_15)],
+    dependencies: [
+        .package(url: "https://github.com/ainame/FrameKit", from: "0.1.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.5.0"),
+    ],
+    targets: [
+        .executableTarget(name: "EasyFrameKitCLI", dependencies: [
+            .product(name: "FrameKit", package: "FrameKit"),
+            .product(name: "ArgumentParser", package: "swift-argument-parser")
+        ]),
+    ],
+    swiftLanguageModes: [.v5]
+)
