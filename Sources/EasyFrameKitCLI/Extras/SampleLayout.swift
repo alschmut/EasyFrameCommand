2f8542028918a3e4b944aee89beb3ba031907671
Alexander Schmutz <alexander.schmutz@partner.bmw.de>
2025-01-27 08:56:38 +0100

    Combine files for SampleLayout

--- Sources/EasyFrameKitCLI/Extras/SampleLayout.swift
+++ Sources/EasyFrameKitCLI/Extras/SampleLayout.swift
@@ -11,4 +11,30 @@ struct SampleLayout: LayoutProvider {
     let textGap: CGFloat
     let textColor: Color
     let backgroundColor: Color
+
+    private static let defaultBackgroundColor = Color(red: 251 / 255, green: 133 / 255, blue: 0 / 255)
+
+    static let iPhone15ProMax = Self(
+        size: CGSize(width: 1290, height: 2796),
+        deviceFrameOffset: .zero,
+        textInsets: EdgeInsets(top: 72, leading: 120, bottom: 0, trailing: 120),
+        imageInsets: EdgeInsets(top: 0, leading: 128, bottom: 72, trailing: 128),
+        keywordFontSize: 148,
+        titleFontSize: 72,
+        textGap: 24,
+        textColor: .white,
+        backgroundColor: defaultBackgroundColor
+    )
+
+    static let iPadPro = Self(
+        size: CGSize(width: 2048, height: 2732),
+        deviceFrameOffset: .zero,
+        textInsets: EdgeInsets(top: 48, leading: 96, bottom: 0, trailing: 96),
+        imageInsets: EdgeInsets(top: 0, leading: 150, bottom: -200, trailing: 150),
+        keywordFontSize: 148,
+        titleFontSize: 72,
+        textGap: 24,
+        textColor: .white,
+        backgroundColor: defaultBackgroundColor
+    )
 }
