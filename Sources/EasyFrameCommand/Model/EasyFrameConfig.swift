d7e145f2f5f96e4fbd075a5dca87854c7e710d24
Alexander Schmutz <aschmutz1@web.de>
2025-01-30 09:05:39 +0100

    Remove unused example swift configuration

--- Sources/EasyFrameCommand/Model/EasyFrameConfig.swift
+++ Sources/EasyFrameCommand/Model/EasyFrameConfig.swift
@@ -14,47 +14,6 @@ struct EasyFrameConfig: Decodable {
 struct ScreenConfig: Decodable {
     let languages: [LanguageConfig]
     let screenshots: [String]
-
-    static func myConfiguration() -> [ScreenConfig] {
-        [
-            ScreenConfig(
-                languages: [
-                    LanguageConfig(locale: "en-GB", title: "Document your crying moments")
-                ],
-                screenshots: ["1-tear-tales-calendar"]
-            ),
-            ScreenConfig(
-                languages: [
-                    LanguageConfig(locale: "en-GB", title: "List")
-                ],
-                screenshots: ["2-tear-tales-list"]
-            ),
-            ScreenConfig(
-                languages: [
-                    LanguageConfig(locale: "en-GB", title: "Statistics")
-                ],
-                screenshots: ["3-statistics"]
-            ),
-            ScreenConfig(
-                languages: [
-                    LanguageConfig(locale: "en-GB", title: "Add records")
-                ],
-                screenshots: ["4-add-record"]
-            ),
-            ScreenConfig(
-                languages: [
-                    LanguageConfig(locale: "en-GB", title: "iCloud and more")
-                ],
-                screenshots: ["5-welcome-sheet"]
-            ),
-            ScreenConfig(
-                languages: [
-                    LanguageConfig(locale: "en-GB", title: "Settings")
-                ],
-                screenshots: ["6-settings"]
-            )
-        ]
-    }
 }
 
 struct LanguageConfig: Decodable {
